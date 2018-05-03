13f03a3dde4b1eb496893c3dcc4129ad2f6948f1
Unity123 <38165550+Unity123@users.noreply.github.com>
2018-05-03 10:49:45 -0600

    Update ext.js

--- ext.js
+++ ext.js
@@ -7,7 +7,7 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
-    ext.get-external-data = function(address, type, callback){
+    ext.get_external_data = function(address, type, callback){
       $.ajax({url: address, dataType: type, success: function(data){
         callback(data);
       }});
@@ -15,7 +15,7 @@
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-          ['R', 'get external data with address %s and type %s', 'get-external-data', '', '']
+          ['R', 'get external data with address %s and type %s', 'get_external_data', '', '']
         ]
     };
 
